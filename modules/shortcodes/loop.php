0359e157538f0baa5b535b3ebb0595e223f07391
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-06-12 18:33:36 +0800

    add heading option to loop shortcode

--- modules/shortcodes/loop.php
+++ modules/shortcodes/loop.php
@@ -22,7 +22,8 @@ if ( ! function_exists( 'wizhi_shortcode_loop' ) ) {
 			'type'   => 'post',
 			'tax'    => 'category',
 			'tag'    => 'default',
-			'tmp'     => 'lists',
+			'tmp'     => '',
+			'heading' => 0,
 			'offset' => 0,
 			'num'    => 8, // 数量: 显示文章数量，-1为全部显示
 		];
@@ -42,6 +43,15 @@ if ( ! function_exists( 'wizhi_shortcode_loop' ) ) {
 			];
 		}
 
+
+		// get term archive name and link
+		$cat = get_term_by( 'slug', $tag, $tax );
+
+		if ( $cat ) {
+			$cat_name = $cat->name;
+			$cat_link = get_term_link( $tag, $tax );
+		}
+
 		// 构建文章查询数组
 		$args = [
 			'post_type'      => $type,
@@ -55,9 +65,30 @@ if ( ! function_exists( 'wizhi_shortcode_loop' ) ) {
 		// 输出
 		$the_query = new WP_Query( $args );
 
-		while ( $the_query->have_posts() ) : $the_query->the_post();
-			echo wizhi_load_template_part( 'content', $tmp );
-		endwhile;
+		if ( $heading == false || empty( $tax ) ) {
+			echo '<div class="sep ui-list-' . $type . $tag . '">';
+			echo '<ul class="ui-list">';
+
+			while ( $the_query->have_posts() ) : $the_query->the_post();
+				echo wizhi_load_template_part( 'content', $tmp );
+			endwhile;
+
+			echo '</ul>';
+			echo '</div>';
+		} else {
+			echo '<div class="ui-box ' . $type . $tag . '">';
+			echo '<div class="ui-box-head">';
+			echo '<h3 class="ui-box-head-title"><a href="' . $cat_link . '">' . $cat_name . '</a></h3>';
+			echo '<a class="ui-box-head-more" href="' . $cat_link . '" target="_blank">更多></a>';
+			echo '</div>';
+			echo '<div class="ui-box-container"><div class="ui-box-content"><ul class="ui-list ui-list-' . $tag . '">';
+
+			while ( $the_query->have_posts() ) : $the_query->the_post();
+				echo wizhi_load_template_part( 'content', $tmp );
+			endwhile;
+
+			echo '</ul></div></div></div>';
+		}
 
 		wp_reset_postdata();
 		wp_reset_query();
