63bf5d93c490b05cbe0f42e0a70562dbb558db96
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-07-27 16:45:27 +0800

    优化 crud 功能

--- inc/crud/model.php
+++ inc/crud/model.php
@@ -1,256 +1,266 @@
 <?php
-	
+
 
 /**
  * Model Class
  *
  */
 class Model {
-	
+
 	private $db;
 	private $table_name;
 	private $columns;
 	private $primary_key;
 	private $pk_is_int;
-	
+
 	/**
-     * Constructor
-     *
-     */
-	public function __construct($table_name) {
-		$this->init($table_name);
+	 * Constructor
+	 *
+	 */
+	public function __construct( $table_name ) {
+		$this->init( $table_name );
 	}
-	
+
 	/**
-     * 设置数据表名称, 列信息和主键
-     *
-     */
-	public function init($table_name) {
-		
+	 * 设置数据表名称, 列信息和主键
+	 *
+	 */
+	public function init( $table_name ) {
+
 		// 设置和数据表
 		global $wpdb;
-		$this->db = $wpdb;
+		$this->db         = $wpdb;
 		$this->table_name = $table_name;
 
 		// 收集列信息
-		$this->db->get_row("SELECT * FROM `$this->table_name`");	//dummy
-		$this->columns = array();
-		foreach ($this->db->get_col_info('name') as $i => $name) {
-			$this->columns[$name] = $this->db->col_info[$i]->type;
+		$this->db->get_row( "SELECT * FROM `$this->table_name`" );
+		$this->columns = [ ];
+
+		foreach ( $this->db->get_col_info( 'name' ) as $i => $name ) {
+			$this->columns[ $name ] = $this->db->col_info[ $i ]->type;
 		}
-		
+
 		// 查找主键、判断是否为整数
-		$row = $this->db->get_row("SHOW FIELDS FROM `$this->table_name` WHERE `Key` = 'PRI'");
+		$row               = $this->db->get_row( "SHOW FIELDS FROM `$this->table_name` WHERE `Key` = 'PRI'" );
 		$this->primary_key = $row->Field;
-		$this->pk_is_int = false;
-		if (stristr($row->Type, 'int')) {
+		$this->pk_is_int   = false;
+
+		if ( stristr( $row->Type, 'int' ) ) {
 			$this->pk_is_int = true;
 		}
 	}
-	
+
 	/**
-     * 返回主键键名
-     *
-     */
+	 * 返回主键键名
+	 *
+	 */
 	public function get_primary_key() {
 		return $this->primary_key;
 	}
-	
+
 	/**
-     * 返回表格名称
-     * 
-     */
+	 * 返回表格名称
+	 *
+	 */
 	public function get_table_name() {
 		return $this->table_name;
 	}
-	
+
 	/**
-     * 返回列名称和类型
-     * 
-     */
+	 * 返回列名称和类型
+	 *
+	 */
 	public function get_columns() {
 		return $this->columns;
 	}
-	
+
 	/**
-     * Returns candidate id for new record
-     * 
-     */
+	 * Returns candidate id for new record
+	 *
+	 */
 	public function get_new_candidate_id() {
 		$new_id = "";
 
 		// autoincrement if pk is integer
-		if ($this->pk_is_int) {
-			$new_id = $this->db->get_var("SELECT MAX(`$this->primary_key`)+1 FROM `$this->table_name`");
+		if ( $this->pk_is_int ) {
+			$new_id = $this->db->get_var( "SELECT MAX(`$this->primary_key`)+1 FROM `$this->table_name`" );
 		} else {
-			$new_id = $this->db->get_var("SELECT MAX(`$this->primary_key`) FROM `$this->table_name`");
+			$new_id = $this->db->get_var( "SELECT MAX(`$this->primary_key`) FROM `$this->table_name`" );
 			$new_id .= NEW_ID_HINT;
 		}
-		if ($new_id == "")	$new_id = "1";
+		if ( $new_id == "" ) {
+			$new_id = "1";
+		}
+
 		return $new_id;
 	}
-	
+
 	/**
-     * 选择所有数据
-     * 
-     */
+	 * 选择所有数据
+	 *
+	 */
 	public function select_all() {
-		return $this->db->get_results("SELECT * FROM `$this->table_name`");
+		return $this->db->get_results( "SELECT * FROM `$this->table_name`" );
 	}
-	
-	/**
-     * 选择合适的数据
-     * 
-     */
-	public function select($key_word, $order_by, $order, $begin_row, $end_row) {
-		
-		$where_qry = $this->generate_where_query($key_word);
-		$order_qry = $this->generate_order_query($order_by, $order);
-		$sql = "SELECT * FROM `$this->table_name` $where_qry $order_qry LIMIT $begin_row, $end_row";
 
-		return $this->db->get_results($sql);
-	}
-	
 	/**
-     * Returns total row count
-     * 
-     */
-	public function count_rows($key_word = "") {
-		
-		$where_qry = $this->generate_where_query($key_word);		
-		$sql = "SELECT COUNT(*) FROM `$this->table_name` $where_qry";
-		
-		return $this->db->get_var($sql);
+	 * 选择合适的数据
+	 *
+	 */
+	public function select( $key_word, $order_by, $order, $begin_row, $end_row ) {
+
+		$where_qry = $this->generate_where_query( $key_word );
+		$order_qry = $this->generate_order_query( $order_by, $order );
+		$sql       = "SELECT * FROM `$this->table_name` $where_qry $order_qry LIMIT $begin_row, $end_row";
+
+		return $this->db->get_results( $sql );
 	}
-	
+
 	/**
-     * Generates where sql query
-     * 
-     */
-     private function generate_where_query($key_word) {
+	 * Returns total row count
+	 *
+	 */
+	public function count_rows( $key_word = "" ) {
+
+		$where_qry = $this->generate_where_query( $key_word );
+		$sql       = "SELECT COUNT(*) FROM `$this->table_name` $where_qry";
+
+		return $this->db->get_var( $sql );
+	}
+
+	/**
+	 * 生成数据库 where 查询
+	 *
+	 */
+	private function generate_where_query( $key_word ) {
 		$qry = "";
-		if ($key_word != "") {
-			$like_statements = array();
-			foreach ($this->columns as $name => $type) {
-				$like_statements[] = $this->db->prepare(" `$name` LIKE '%%%s%%'", $key_word);
+		if ( $key_word != "" ) {
+			$like_statements = [ ];
+			foreach ( $this->columns as $name => $type ) {
+				$like_statements[] = $this->db->prepare( " `$name` LIKE '%%%s%%'", $key_word );
 			}
-			$qry = " WHERE " . implode(" OR ", $like_statements);
+			$qry = " WHERE " . implode( " OR ", $like_statements );
 		}
-		return $qry;
-	 }
-	 
-	/**
-     * Generates order by sql query
-     * 
-     */
-     private function generate_order_query($order_by, $order) {
-     	$qry = "";
-		if ($order_by != "") {
-			$order = esc_sql($order);
-			$order_by = esc_sql($order_by);
-			$qry = " ORDER BY `$order_by` $order";
-		}
-		return $qry;
-	 }
-	 
-	/**
-     * Returns single row
-     *
-     */
-	public function get_row($id) {
-		$sql = $this->db->prepare("SELECT * FROM `$this->table_name` WHERE `$this->primary_key` = '%s'", $id);
-		return $this->db->get_row($sql);
-	}
-	
-	/**
-     * Adds new record
-     *
-     */
-	public function insert($vals) {
-		
-		// collect insert values and strip slashes
-		$insert_vals = array();
-		foreach ($this->columns as $name => $type) {
-			$insert_vals[$name] = stripslashes_deep($vals[$name]);
-		}
-		
-		// check if pk already exists
-		$sql = $this->db->prepare("SELECT `$this->primary_key` FROM `$this->table_name` WHERE `$this->primary_key` = '%s'", $insert_vals[$this->primary_key]);
-		$exists = $this->db->get_var($sql);
 
-		// insert
-		if ($exists == "") {
-			if ($this->db->insert($this->table_name, $insert_vals)) {
-				return $insert_vals[$this->primary_key];
+		return $qry;
+	}
+
+	/**
+	 * 生成排序查询
+	 *
+	 */
+	private function generate_order_query( $order_by, $order ) {
+		$qry = "";
+		if ( $order_by != "" ) {
+			$order    = esc_sql( $order );
+			$order_by = esc_sql( $order_by );
+			$qry      = " ORDER BY `$order_by` $order";
+		}
+
+		return $qry;
+	}
+
+	/**
+	 * 返回一行数据
+	 *
+	 */
+	public function get_row( $id ) {
+		$sql = $this->db->prepare( "SELECT * FROM `$this->table_name` WHERE `$this->primary_key` = '%s'", $id );
+
+		return $this->db->get_row( $sql );
+	}
+
+	/**
+	 * 添加新记录
+	 *
+	 */
+	public function insert( $vals ) {
+
+		// 收集需要插入的数据, 处理 /
+		$insert_vals = [ ];
+		foreach ( $this->columns as $name => $type ) {
+			$insert_vals[ $name ] = stripslashes_deep( $vals[ $name ] );
+		}
+
+		// 检查主键是否已存在
+		$sql    = $this->db->prepare( "SELECT `$this->primary_key` FROM `$this->table_name` WHERE `$this->primary_key` = '%s'", $insert_vals[ $this->primary_key ] );
+		$exists = $this->db->get_var( $sql );
+
+		// 插入数据库
+		if ( $exists == "" ) {
+			if ( $this->db->insert( $this->table_name, $insert_vals ) ) {
+				return $insert_vals[ $this->primary_key ];
 			}
 		}
+
 		return "";
 	}
-	
+
 	/**
-     * Updates record
-     *
-     */
-	public function update($vals) {
-		
-		// collect update values and strip slashes
-		$update_vals = array();
-		foreach ($this->columns as $name => $type) {
-			$update_vals[$name] = stripslashes_deep($vals[$name]);
+	 * 更新记录
+	 *
+	 */
+	public function update( $vals ) {
+
+		// 收集需要更新的数据, 处理 /
+		$update_vals = [ ];
+		foreach ( $this->columns as $name => $type ) {
+			$update_vals[ $name ] = stripslashes_deep( $vals[ $name ] );
 		}
-		
-		// update
-		if ($this->db->update($this->table_name, $update_vals, array($this->primary_key => $vals[$this->primary_key]))) {
+
+		// 更新记录
+		if ( $this->db->update( $this->table_name, $update_vals, [ $this->primary_key => $vals[ $this->primary_key ] ] ) ) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	/**
-     * Deletes record
-     *
-     */
-	public function delete($id) {
-		$sql = $this->db->prepare("DELETE FROM `$this->table_name` WHERE `$this->primary_key` = '%s'", $id);
-		if ($this->db->query($sql)) {
+	 * 删除记录
+	 *
+	 */
+	public function delete( $id ) {
+		$sql = $this->db->prepare( "DELETE FROM `$this->table_name` WHERE `$this->primary_key` = '%s'", $id );
+		if ( $this->db->query( $sql ) ) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	/**
-     * Checks validity of a table
-     *
-     */
-	public function validate($table_name) {
-		
-		// gather column information & verify errors if any
+	 * 检查数据表的正确性
+	 *
+	 */
+	public function validate( $table_name ) {
+
+		// 收集列信息并检查错误
 		$err_msg = "";
-		$results = $this->db->get_results("SHOW KEYS FROM `$table_name` WHERE `Key_name` = 'PRIMARY'");
-		if (1 < $this->db->num_rows) {
+		$results = $this->db->get_results( "SHOW KEYS FROM `$table_name` WHERE `Key_name` = 'PRIMARY'" );
+		if ( 1 < $this->db->num_rows ) {
 			$err_msg = "Error: table $table_name has multiple primary keys";
-		
-		} else if ($results[0]->Seq_in_index != 1) {
+
+		} else if ( $results[ 0 ]->Seq_in_index != 1 ) {
 			$err_msg = "Error: table $table_name's primary key is not set at first column";
-		
+
 		}
-		
+
 		return $err_msg;
 	}
-	
+
 	/**
-     * Get list of available tables in schema
-     *
-     */
-    public function get_table_options() {
-    	$options = array();
-    	foreach ($this->db->get_results("SHOW TABLES") as $row ){
-			foreach ($row as $k => $v){
+	 * 获取可用的数据表为schema
+	 *
+	 */
+	public function get_table_options() {
+		$options = [ ];
+		foreach ( $this->db->get_results( "SHOW TABLES" ) as $row ) {
+			foreach ( $row as $k => $v ) {
 				$options[] = $v;
 			}
 		}
+
 		return $options;
 	}
 }
--- inc/crud/view/edit.tpl
+++ inc/crud/view/edit.tpl
@@ -1,39 +1,36 @@
 <div class='wrap'>
-<h2>Simple Table Manager - Edit</h2>
-<h3><?php echo $table_name ?></h3>
+    <h1>
+        编辑数据
+        <a class="page-title-action" href='<?php echo $this->url['list'] ?>'>返回列表</a>
+    </h1>
 
-<?php
-	if ($status == "error") {
-		echo "<div class='error'><p>$message</p></div>";
-	} else if ($status == "success") {
-		echo "<div class='updated'><p>$message</p></div>";
-	}
-?>
+    <?php
+        if ($status == "error") {
+            echo "<div class='error'><p>$message</p></div>";
+        } else if ($status == "success") {
+            echo "<div class='updated'><p>$message</p></div>";
+        }
+    ?>
 
-	<div class='subsubsub'>
-	<a href="<?php echo $this->url['list'] ?>">&lt;&lt; Return to list</a>
-	</div>
-
-<?php if (!empty($row)) { ?>
-		<form method='post' action='<?php echo $this->url['edit'] ?>'>
-		<table class='wp-list-table widefat fixed'>
-<?php
-		require_once("util.php");
-		foreach ($row as $name => $value) {
-			if ($name == $primary_key) {
-//				echo "<tr><th class='simple-table-manager'>$name *</th><td>" . data_type2html_input($columns[$name], $name, $value) . "</td></tr>";
-				echo "<tr><th class='simple-table-manager'>$name *</th><td><input type='text' readonly='readonly' name='$name' value='$value'/></td></tr>";
-			} else {
-				echo "<tr><th class='simple-table-manager'>$name</th><td>" . data_type2html_input($columns[$name], $name, $value) . "</td></tr>";
-			}
-		}
-?>
-		</table>
-		<div class="tablenav bottom">
-		<input type='submit' name='update' value='Update' class='button'>&nbsp;
-		<input type='submit' name='delete' value='Delete' class='button' onclick="return confirm('Are you sure you want to delete this record?')">
-		</div>
-		<input type="hidden" name="id" value="<?php echo $id ?>">
-		</form>
-<?php } ?>
+    <?php if (!empty($row)) { ?>
+        <form method='post' action='<?php echo $this->url['edit'] ?>'>
+        <table class='wp-list-table widefat fixed'>
+            <?php
+                require_once("util.php");
+                foreach ($row as $name => $value) {
+                    if ($name == $primary_key) {
+                        echo "<tr><th class='simple-table-manager'>$name *</th><td><input type='text' readonly='readonly' name='$name' value='$value'/></td></tr>";
+                    } else {
+                        echo "<tr><th class='simple-table-manager'>$name</th><td>" . data_type2html_input($columns[$name], $name, $value) . "</td></tr>";
+                    }
+                }
+            ?>
+        </table>
+        <div class="tablenav bottom">
+        <input type='submit' name='update' value='更新' class='button'>&nbsp;
+        <input type='submit' name='delete' value='删除' class='button' onclick="return confirm('确定要删除吗? ?')">
+        </div>
+        <input type="hidden" name="id" value="<?php echo $id ?>">
+        </form>
+    <?php } ?>
 </div>
