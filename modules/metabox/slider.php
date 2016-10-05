512436200f5f663ac50fd66695f1157d4fcadca7
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-10-06 07:24:45 +0800

    slider 自定义链接问题

--- modules/metabox/slider.php
+++ modules/metabox/slider.php
@@ -3,5 +3,5 @@
 add_action( 'after_setup_theme', 'wizhi_cms_slider_meta' );
 function wizhi_cms_slider_meta() {
 	$fm = new Fieldmanager_Textfield( [ 'name' => '_link_url', 'label' => __( 'Custom link', 'wizhi' ) ] );
-	$fm->add_meta_box( __( 'Custom link', 'wizhi' ), 'post' );
+	$fm->add_meta_box( __( 'Custom link', 'wizhi' ), 'slider' );
 }
