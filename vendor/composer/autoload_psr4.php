7211351e6f29de65cbbee18f4c04437990ae0996
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-12-31 16:42:14 +0800

    add post type class

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'TheFold\\WordPress\\' => array($vendorDir . '/thefold/wordpress-dispatcher'),
+    'PostTypes\\' => array($vendorDir . '/jjgrainger/posttypes/src'),
 );
