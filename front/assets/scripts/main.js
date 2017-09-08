c5d0a9927794eb1ec9a558f494b559f565cb0812
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2017-09-08 20:30:15 +0800

    添加 Captcha 验证码，有待测试

--- front/assets/scripts/main.js
+++ front/assets/scripts/main.js
@@ -6,17 +6,27 @@ jQuery(document).ready(function ($) {
     });
 
     // 商品相册
-    $('.md-gallery').each(function() {
+    $('.md-gallery').each(function () {
         $(this).magnificPopup({
             delegate: 'a',
-            type: 'image',
-            gallery: {
-                enabled:true,
+            type    : 'image',
+            gallery : {
+                enabled           : true,
                 navigateByImgClick: true,
-                preload: [0,1]
+                preload           : [0,
+                    1]
             }
         });
     });
+
+
+    /**
+     * 刷新验证码
+     * @param obj
+     */
+    function refresh_code(obj) {
+        obj.src = obj.src + "?code=" + Math.random();
+    }
 
 
     // 页脚总是显示在底部
@@ -26,7 +36,7 @@ jQuery(document).ready(function ($) {
         windowH = $window.height(),
         wrapperH = $wrapper.height();
 
-    if ( $footer.length > 0 && $wrapper.has('#colophon')) {
+    if ($footer.length > 0 && $wrapper.has('#colophon')) {
         if (windowH > wrapperH) {
             $footer.css({'margin-top': ( windowH - wrapperH + 50 )});
             return false;
