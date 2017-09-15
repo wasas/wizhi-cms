9d77d232beca6bea61ddca45ed81b789511cb477
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2017-09-15 19:04:39 +0800

    添加获取用户真实IP 的方法

--- src/helpers.php
+++ src/helpers.php
@@ -48,6 +48,29 @@ function is_ajax() {
 }
 
 
+
+/**
+ * 获取用户的真实 IP
+ *
+ * @return mixed
+ */
+function get_ip() {
+	$client  = @$_SERVER[ 'HTTP_CLIENT_IP' ];
+	$forward = @$_SERVER[ 'HTTP_X_FORWARDED_FOR' ];
+	$remote  = $_SERVER[ 'REMOTE_ADDR' ];
+
+	if ( filter_var( $client, FILTER_VALIDATE_IP ) ) {
+		$ip = $client;
+	} elseif ( filter_var( $forward, FILTER_VALIDATE_IP ) ) {
+		$ip = $forward;
+	} else {
+		$ip = $remote;
+	}
+
+	return $ip;
+}
+
+
 if ( ! function_exists( 'dd' ) ) {
 	/**
 	 * 输出传入的变量并结束程序
